71e90fb2b8c80bf6cb787ef51f590f2af7c51de8
Jihee <42454759+sjh709@users.noreply.github.com>
2024-04-26 02:50:32 +0900

    [Bronze V] Title: 시험 성적, Time: 128 ms, Memory: 9328 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.js
+++ 백준/Bronze/9498. 시험 성적/시험 성적.js
@@ -1,22 +1,15 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const fs = require("fs");
+const line = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-rl.on("line", function (line) {
-  const score = Number(line);
-  if (score >= 90 && score <= 100) {
-    console.log("A");
-  } else if (score >= 80 && score <= 89) {
-    console.log("B");
-  } else if (score >= 70 && score <= 79) {
-    console.log("C");
-  } else if (score >= 60 && score <= 69) {
-    console.log("D");
-  } else {
-    console.log("F");
-  }
-}).on("close", function () {
-  process.exit();
-});
+const score = Number(line[0]);
+if (score >= 90 && score <= 100) {
+  console.log("A");
+} else if (score >= 80 && score <= 89) {
+  console.log("B");
+} else if (score >= 70 && score <= 79) {
+  console.log("C");
+} else if (score >= 60 && score <= 69) {
+  console.log("D");
+} else {
+  console.log("F");
+}
