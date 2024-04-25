0358f3a9c487e6bbd687709dbba01d66fd0ea681
Jihee <42454759+sjh709@users.noreply.github.com>
2024-04-26 03:40:40 +0900

    [Bronze III] Title: 오븐 시계, Time: 124 ms, Memory: 9592 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.js
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.js
@@ -1,26 +1,16 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const fs = require("fs");
+const line = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let input = [];
-rl.on("line", function (line) {
-  input.push(line);
-}).on("close", function () {
-  let h = Number(input[0].split(" ")[0]);
-  let m = Number(input[0].split(" ")[1]);
-  const time = Number(input[1]);
+let h = Number(line[0].split(" ")[0]);
+let m = Number(line[0].split(" ")[1]);
+const time = Number(line[1]);
 
-  const totalTime = h * 60 + m + time;
+const totalTime = h * 60 + m + time;
 
-  h =
-    parseInt(totalTime / 60) > 23
-      ? parseInt(totalTime / 60) - 24
-      : parseInt(totalTime / 60);
-  m = totalTime % 60;
+h =
+  parseInt(totalTime / 60) > 23
+    ? parseInt(totalTime / 60) - 24
+    : parseInt(totalTime / 60);
+m = totalTime % 60;
 
-  console.log(h, m);
-
-  process.exit();
-});
+console.log(h, m);
