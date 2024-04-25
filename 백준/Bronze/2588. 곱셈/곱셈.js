cee87ae44c8047c942a507390a989fe6eabefdbb
Jihee <42454759+sjh709@users.noreply.github.com>
2024-04-25 19:59:29 +0900

    [Bronze III] Title: 곱셈, Time: 120 ms, Memory: 9584 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.js
+++ 백준/Bronze/2588. 곱셈/곱셈.js
@@ -1,20 +1,10 @@
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const fs = require("fs");
+const line = fs.readFileSync("/dev/stdin").toString().split("\n");
 
-let input = [];
-rl.on("line", function (line) {
-  input.push(Number(line));
-}).on("close", function () {
-  const a = input[0];
-  const b = input[1];
+const a = Number(line[0]);
+const b = Number(line[1]);
 
-  console.log(a * (b % 10));
-  console.log(a * parseInt((b % 100) / 10));
-  console.log(a * parseInt(b / 100));
-  console.log(a * b);
-
-  process.exit();
-});
+console.log(a * (b % 10));
+console.log(a * parseInt((b % 100) / 10));
+console.log(a * parseInt(b / 100));
+console.log(a * b);
