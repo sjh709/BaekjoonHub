a02a591ecccf6e4e6dfd81b5f10a12aa976e2454
Jihee <42454759+sjh709@users.noreply.github.com>
2024-05-04 16:47:11 +0900

    [Silver V] Title: 소트인사이드, Time: 124 ms, Memory: 9332 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.js
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.js
@@ -1,11 +1,18 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const solution = (line) => {
-  const answer = line
-    .split('')
-    .sort((a, b) => b - a)
-    .join('');
+const solution = (n) => {
+  let cnt = Array(10).fill(0);
+  for (let x of n) {
+    cnt[Number(x)]++;
+  }
+
+  let answer = '';
+  for (let i = 9; i >= 0; i--) {
+    for (let j = 0; j < cnt[i]; j++) {
+      answer += i + '';
+    }
+  }
   console.log(answer);
 };
 
