7b1f21a423761c8d531664f3b9f810d38b097ad3
Jihee <42454759+sjh709@users.noreply.github.com>
2024-05-08 15:36:13 +0900

    [Silver III] Title: 주유소, Time: 244 ms, Memory: 19576 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -0,0 +1,31 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const solution = (input) => {
+  const n = Number(input[0]);
+  const dist = input[1].split(' ').map(Number);
+  const cost = input[2].split(' ').map(Number);
+
+  let minCost = cost[0];
+  for (let i = 0; i < n; i++) {
+    minCost = Math.min(minCost, cost[i]);
+    cost[i] = minCost;
+  }
+
+  let answer = 0;
+  for (let i = 0; i < n - 1; i++) {
+    answer += cost[i] * dist[i];
+  }
+  console.log(answer);
+};
+
+const input = [];
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  solution(input);
+  process.exit();
+});
