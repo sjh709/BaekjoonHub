6d7498bbe88767412e348c125ecb6ad5b4b3c33d
Jihee <42454759+sjh709@users.noreply.github.com>
2024-05-10 16:33:08 +0900

    [Gold V] Title: 회문, Time: 892 ms, Memory: 263396 KB -BaekjoonHub

---
+++ 백준/Gold/17609. 회문/회문.js
@@ -0,0 +1,41 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const palindrome = (s) => {
+  return s === s.split('').reverse().join('');
+};
+
+const solution = (input) => {
+  const t = Number(input[0]);
+  for (let i = 1; i <= t; i++) {
+    let s = input[i];
+    if (palindrome(s)) {
+      console.log(0);
+    } else {
+      const len = s.length;
+      for (let i = 0; i < parseInt(len / 2); i++) {
+        if (s[i] !== s[len - i - 1]) {
+          const s1 = s.slice(0, i) + s.slice(i + 1, len);
+          const s2 = s.slice(0, len - i - 1) + s.slice(len - i, len);
+          if (palindrome(s1) || palindrome(s2)) {
+            console.log(1);
+          } else {
+            console.log(2);
+          }
+          break;
+        }
+      }
+    }
+  }
+};
+
+const input = [];
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  solution(input);
+  process.exit();
+});
