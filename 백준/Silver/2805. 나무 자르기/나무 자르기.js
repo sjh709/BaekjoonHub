274c35728d33bd6551b0e01233135ae6302937de
Jihee <42454759+sjh709@users.noreply.github.com>
2024-05-12 01:57:10 +0900

    [Silver II] Title: 나무 자르기, Time: 972 ms, Memory: 134944 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.js
@@ -0,0 +1,39 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const solution = (input) => {
+  const [n, m] = input[0].split(' ').map(Number);
+  const arr = input[1].split(' ').map(Number);
+
+  let start = 1;
+  let end = arr.reduce((acc, curr) => Math.max(acc, curr), 0);
+  let result = 0;
+  while (start <= end) {
+    let mid = parseInt((start + end) / 2);
+    let total = 0;
+
+    for (let x of arr) {
+      total += x - Math.min(mid, x);
+    }
+
+    if (total >= m) {
+      start = mid + 1;
+      result = mid;
+    } else {
+      end = mid - 1;
+    }
+  }
+
+  console.log(result);
+};
+
+const input = [];
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  solution(input);
+  process.exit();
+});
