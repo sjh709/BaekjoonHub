5c54713b4d1475618dc683af6bf4446f58d16eb9
Jihee <42454759+sjh709@users.noreply.github.com>
2024-05-14 01:40:45 +0900

    [Silver III] Title: N과 M (1), Time: 232 ms, Memory: 17388 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.js
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.js
@@ -1,8 +1,5 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const solution = (input) => {
   const [n, m] = input[0].split(' ').map(Number);
@@ -29,11 +26,4 @@ const solution = (input) => {
   console.log(answer);
 };
 
-const input = [];
-rl.on('line', function (line) {
-  input.push(line);
-  rl.close();
-}).on('close', function () {
-  solution(input);
-  process.exit();
-});
+solution(input);
