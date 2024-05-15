3d43918d13609577ab213de59536561ea3460c32
Jihee <42454759+sjh709@users.noreply.github.com>
2024-05-16 02:52:30 +0900

    [Silver II] Title: 외판원 순회 2, Time: 236 ms, Memory: 10116 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.js
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.js
@@ -3,49 +3,47 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const solution = (input) => {
   const n = Number(input[0]);
-  const dist = [];
-  const city = [];
-  const visited = new Array(n).fill(false);
+  let graph = [];
+  for (let i = 0; i <= n; i++) graph.push([0]);
   for (let i = 1; i <= n; i++) {
-    city.push(i);
-    dist.push(input[i].split(' ').map(Number));
+    const line = input[i].split(' ').map(Number);
+    for (let j = 0; j < n; j++) graph[i].push(line[j]);
   }
+  let visited = new Array(11).fill(false);
+  let result = [];
+  let minValue = 1e9;
 
-  let answer = Number.MAX_SAFE_INTEGER;
-  const solve = (arr) => {
-    let sum = 0;
-    for (let i = 0; i < arr.length; i++) {
-      if (i === arr.length - 1) {
-        if (dist[arr[i] - 1][arr[0] - 1] === 0) return;
-        sum += dist[arr[i] - 1][arr[0] - 1];
-      } else {
-        if (dist[arr[i] - 1][arr[i + 1] - 1] === 0) return;
-        sum += dist[arr[i] - 1][arr[i + 1] - 1];
-      }
-    }
-    answer = Math.min(answer, sum);
-  };
-
-  let selected = [];
   const dfs = (depth) => {
-    if (depth === n) {
-      solve(selected);
+    if (depth === n - 1) {
+      let totalCost = 0;
+      let cur = 1;
+      for (let i = 0; i < n - 1; i++) {
+        let nextNode = result[i];
+        let cost = graph[cur][nextNode];
+        if (cost === 0) return;
+        totalCost += cost;
+        cur = nextNode;
+      }
+      let cost = graph[cur][1];
+      if (cost === 0) return;
+      totalCost += cost;
+      minValue = Math.min(minValue, totalCost);
       return;
     }
 
-    for (let i = 0; i < city.length; i++) {
+    for (let i = 2; i <= n; i++) {
       if (visited[i]) continue;
-      selected.push(city[i]);
+      result.push(i);
       visited[i] = true;
       dfs(depth + 1);
-      selected.pop();
+      result.pop();
       visited[i] = false;
     }
   };
 
   dfs(0);
 
-  console.log(answer);
+  console.log(minValue);
 };
 
 solution(input);
