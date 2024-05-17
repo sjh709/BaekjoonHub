042ba0dabb0d42f15ee6eac5f6c8a9c7c9f166b2
Jihee <42454759+sjh709@users.noreply.github.com>
2024-05-17 19:07:55 +0900

    [Silver I] Title: 부등호, Time: 552 ms, Memory: 13476 KB -BaekjoonHub

---
+++ 백준/Silver/2529. 부등호/부등호.js
@@ -0,0 +1,68 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const solution = (input) => {
+  const k = Number(input[0]);
+  const arr = input[1].split(' ');
+
+  let min = Number.MAX_SAFE_INTEGER;
+  let max = 0;
+
+  let selected = [];
+  let visited = new Array(10).fill(false);
+  const dfs = (depth) => {
+    if (depth === k + 1) {
+      let flag = true;
+      for (let i = 0; i < k; i++) {
+        if (arr[i] === '<') {
+          if (selected[i] >= selected[i + 1]) {
+            flag = false;
+            break;
+          }
+        } else {
+          if (selected[i] <= selected[i + 1]) {
+            flag = false;
+            break;
+          }
+        }
+      }
+
+      if (flag) {
+        const result = Number(selected.join(''));
+        min = Math.min(min, result);
+        max = Math.max(max, result);
+      }
+
+      return;
+    }
+
+    for (let i = 0; i < 10; i++) {
+      if (visited[i]) continue;
+      selected.push(i);
+      visited[i] = true;
+      dfs(depth + 1);
+      selected.pop();
+      visited[i] = false;
+    }
+  };
+
+  dfs(0);
+
+  console.log(max);
+  console.log(
+    String(min).length < k + 1
+      ? '0'.repeat(k + 1 - String(min).length) + min
+      : min
+  );
+};
+
+const input = [];
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  solution(input);
+  process.exit();
+});
