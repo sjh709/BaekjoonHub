fdc00f9191e8392c62efebdb2e69ee863da9d36b
Jihee <42454759+sjh709@users.noreply.github.com>
2024-05-19 03:56:52 +0900

    [Gold IV] Title: 트리, Time: 588 ms, Memory: 43208 KB -BaekjoonHub

--- 백준/Gold/4803. 트리/트리.js
+++ 백준/Gold/4803. 트리/트리.js
@@ -1,8 +1,5 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const solution = (input) => {
   let line = 0;
@@ -51,10 +48,4 @@ const solution = (input) => {
   }
 };
 
-const input = [];
-rl.on('line', function (line) {
-  input.push(line);
-}).on('close', function () {
-  solution(input);
-  process.exit();
-});
+solution(input);
