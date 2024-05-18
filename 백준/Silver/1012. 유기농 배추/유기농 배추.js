2f50edc6dccd3cfffe6608031e9b15c62a9c88ba
Jihee <42454759+sjh709@users.noreply.github.com>
2024-05-18 19:05:32 +0900

    [Silver II] Title: 유기농 배추, Time: 188 ms, Memory: 13188 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -0,0 +1,55 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const solution = (input) => {
+  const t = Number(input[0]);
+  let line = 1;
+  for (let i = 0; i < t; i++) {
+    const [m, n, k] = input[line].split(' ').map(Number);
+    const graph = Array.from({ length: n }, () => new Array(m).fill(0));
+    const visited = Array.from({ length: n }, () => new Array(m).fill(false));
+    for (let j = line + 1; j <= line + k; j++) {
+      const [x, y] = input[j].split(' ').map(Number);
+      graph[y][x] = 1;
+    }
+
+    const dx = [-1, 1, 0, 0];
+    const dy = [0, 0, -1, 1];
+    const dfs = (graph, x, y, visited) => {
+      visited[x][y] = true;
+      for (let i = 0; i < 4; i++) {
+        let nx = x + dx[i];
+        let ny = y + dy[i];
+        if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+        if (visited[nx][ny]) continue;
+        if (graph[nx][ny] === 0) continue;
+        dfs(graph, nx, ny, visited);
+      }
+    };
+
+    let cnt = 0;
+    for (let i = 0; i < n; i++) {
+      for (let j = 0; j < m; j++) {
+        if (!visited[i][j] && graph[i][j] === 1) {
+          dfs(graph, i, j, visited);
+          cnt += 1;
+        }
+      }
+    }
+
+    console.log(cnt);
+
+    line += k + 1;
+  }
+};
+
+const input = [];
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  solution(input);
+  process.exit();
+});
