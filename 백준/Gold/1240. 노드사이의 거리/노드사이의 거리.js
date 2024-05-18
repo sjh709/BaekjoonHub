0c2f2e2904b66c9549e2d50cecbb8ef902c974f3
Jihee <42454759+sjh709@users.noreply.github.com>
2024-05-19 03:06:56 +0900

    [Gold V] Title: 노드사이의 거리, Time: 396 ms, Memory: 15064 KB -BaekjoonHub

---
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.js
@@ -0,0 +1,41 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const solution = (input) => {
+  const [n, m] = input[0].split(' ').map(Number);
+  let graph = [];
+  for (let i = 1; i <= n; i++) graph[i] = [];
+  let visited;
+  let distance;
+  for (let i = 1; i < n; i++) {
+    const [x, y, cost] = input[i].split(' ').map(Number);
+    graph[x].push([y, cost]);
+    graph[y].push([x, cost]);
+  }
+
+  const dfs = (x, dist) => {
+    if (visited[x]) return;
+    visited[x] = true;
+    distance[x] = dist;
+    for (let [y, cost] of graph[x]) dfs(y, dist + cost);
+  };
+
+  for (let i = 0; i < m; i++) {
+    const [x, y] = input[n + i].split(' ').map(Number);
+    visited = new Array(n + 1).fill(false);
+    distance = new Array(n + 1).fill(-1);
+    dfs(x, 0);
+    console.log(distance[y]);
+  }
+};
+
+const input = [];
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  solution(input);
+  process.exit();
+});
