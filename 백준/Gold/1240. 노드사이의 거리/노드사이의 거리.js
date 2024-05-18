1042580f89a7f261ffc619a06c199c6114c3b0a3
Jihee <42454759+sjh709@users.noreply.github.com>
2024-05-19 03:07:51 +0900

    [Gold V] Title: 노드사이의 거리, Time: 396 ms, Memory: 14936 KB -BaekjoonHub

--- 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.js
+++ 백준/Gold/1240. 노드사이의 거리/노드사이의 거리.js
@@ -1,8 +1,5 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const solution = (input) => {
   const [n, m] = input[0].split(' ').map(Number);
@@ -32,10 +29,4 @@ const solution = (input) => {
   }
 };
 
-const input = [];
-rl.on('line', function (line) {
-  input.push(line);
-}).on('close', function () {
-  solution(input);
-  process.exit();
-});
+solution(input);
