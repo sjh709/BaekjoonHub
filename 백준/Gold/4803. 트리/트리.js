9dbc2fbda62f761d7b8c27514dd0da20d6a7154a
Jihee <42454759+sjh709@users.noreply.github.com>
2024-05-19 03:56:01 +0900

    [Gold IV] Title: 트리, Time: 616 ms, Memory: 61416 KB -BaekjoonHub

---
+++ 백준/Gold/4803. 트리/트리.js
@@ -0,0 +1,60 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const solution = (input) => {
+  let line = 0;
+  let idx = 1;
+  while (true) {
+    const [n, m] = input[line].split(' ').map(Number);
+    if (n === 0 && m === 0) break;
+
+    let graph = [];
+    for (let i = 1; i <= n; i++) graph[i] = [];
+    let visited = new Array(n + 1).fill(false);
+
+    for (let i = 1; i <= m; i++) {
+      const [x, y] = input[line + i].split(' ').map(Number);
+      graph[x].push(y);
+      graph[y].push(x);
+    }
+
+    const isCycle = (x, prev) => {
+      visited[x] = true;
+      for (let y of graph[x]) {
+        if (!visited[y]) {
+          if (isCycle(y, x)) return true;
+        } else if (y !== prev) return true;
+      }
+      return false;
+    };
+
+    let cnt = 0;
+    for (let i = 1; i <= n; i++) {
+      if (!visited[i]) {
+        if (!isCycle(i, 0)) cnt++;
+      }
+    }
+
+    if (cnt === 0) {
+      console.log('Case ' + idx + ': No trees.');
+    } else if (cnt === 1) {
+      console.log('Case ' + idx + ': There is one tree.');
+    } else {
+      console.log('Case ' + idx + ': A forest of ' + cnt + ' trees.');
+    }
+
+    idx += 1;
+    line += m + 1;
+  }
+};
+
+const input = [];
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  solution(input);
+  process.exit();
+});
