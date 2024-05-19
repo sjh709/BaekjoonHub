38020c85c6b44c97a3616d7c4bda44c3a8a96f93
Jihee <42454759+sjh709@users.noreply.github.com>
2024-05-20 05:05:46 +0900

    [Silver I] Title: 단지번호붙이기, Time: 120 ms, Memory: 9376 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.js
@@ -3,49 +3,36 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const solution = (input) => {
   const n = Number(input[0]);
-  const arr = [];
+  const graph = [];
   for (let i = 1; i <= n; i++) {
-    arr.push(input[i].split('').map(Number));
+    graph.push(input[i].split('').map(Number));
   }
 
-  let visited = new Array(n);
-  for (let i = 0; i < n; i++) visited[i] = new Array(n).fill(false);
-
-  const dx = [-1, 1, 0, 0];
-  const dy = [0, 0, -1, 1];
-  let cnt = 1;
-  let answer = 0;
   const dfs = (x, y) => {
-    visited[x][y] = true;
-    for (let i = 0; i < 4; i++) {
-      let nx = x + dx[i];
-      let ny = y + dy[i];
-      if (nx < 0 || nx >= n || ny < 0 || ny >= n) continue;
-      if (visited[nx][ny]) continue;
-      if (arr[nx][ny] === 0) continue;
-      cnt += 1;
-      dfs(nx, ny);
+    if (x <= -1 || x >= n || y <= -1 || y >= n) return 0;
+    if (graph[x][y] >= 1) {
+      graph[x][y] = -1;
+      let result = 1;
+      result += dfs(x - 1, y);
+      result += dfs(x, y - 1);
+      result += dfs(x + 1, y);
+      result += dfs(x, y + 1);
+      return result;
     }
+    return 0;
   };
 
-  let result = [];
+  let answer = [];
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < n; j++) {
-      if (!visited[i][j] && arr[i][j] === 1) {
-        cnt = 1;
-        dfs(i, j);
-        result.push(cnt);
-        answer += 1;
-      }
+      let current = dfs(i, j);
+      if (current > 0) answer.push(current);
     }
   }
 
-  result.sort((a, b) => a - b);
+  answer.sort((a, b) => a - b);
 
-  console.log(answer);
-  for (let x of result) {
-    console.log(x);
-  }
+  console.log(answer.length + '\n' + answer.join('\n'));
 };
 
 solution(input);
