97a176a75bc54c139e3dafe394322314708884de
Jihee <42454759+sjh709@users.noreply.github.com>
2024-05-20 04:23:45 +0900

    [Gold V] Title: 치킨 배달, Time: 248 ms, Memory: 13428 KB -BaekjoonHub

---
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.js
@@ -0,0 +1,58 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const solution = (input) => {
+  const [n, m] = input[0].split(' ').map(Number);
+  const chicken = [];
+  const house = [];
+  for (let i = 1; i <= n; i++) {
+    let line = input[i].split(' ').map(Number);
+    for (let j = 0; j < n; j++) {
+      if (line[j] === 1) house.push([i, j]);
+      if (line[j] === 2) chicken.push([i, j]);
+    }
+  }
+
+  let visited = new Array(chicken.length).fill(false);
+  let selected = [];
+  let answer = 1e9;
+  const dfs = (depth, start) => {
+    if (depth === m) {
+      let result = [];
+      for (let i of selected) result.push(chicken[i]);
+      let sum = 0;
+      for (let [hx, hy] of house) {
+        let temp = 1e9;
+        for (let [cx, cy] of result) {
+          temp = Math.min(temp, Math.abs(hx - cx) + Math.abs(hy - cy));
+        }
+        sum += temp;
+      }
+      answer = Math.min(answer, sum);
+      return;
+    }
+    for (let i = start; i < chicken.length; i++) {
+      if (visited[i]) continue;
+      selected.push(i);
+      visited[i] = true;
+      dfs(depth + 1, i + 1);
+      selected.pop();
+      visited[i] = false;
+    }
+  };
+
+  dfs(0, 0);
+
+  console.log(answer);
+};
+
+const input = [];
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  solution(input);
+  process.exit();
+});
