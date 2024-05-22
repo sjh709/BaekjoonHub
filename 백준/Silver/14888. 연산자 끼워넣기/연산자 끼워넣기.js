da956f2403a86b7ae174dbcb6c0de9f007923175
Jihee <42454759+sjh709@users.noreply.github.com>
2024-05-23 04:06:16 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 188 ms, Memory: 10412 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.js
@@ -4,57 +4,43 @@ const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 const solution = (input) => {
   let n = Number(input[0]);
   let arr = input[1].split(' ').map(Number);
-  let temp = input[2].split(' ').map(Number);
-  let flag = [];
-  for (let i = 0; i < 4; i++) {
-    for (let j = 0; j < temp[i]; j++) {
-      flag.push(i);
-    }
-  }
+  let [add, sub, mul, div] = input[2].split(' ').map(Number);
 
-  let selected = [];
-  let visited = new Array(n - 1).fill(false);
-  let min = 1e9 + 1;
-  let max = -(1e9 + 1);
-  const dfs = (depth) => {
-    if (depth === n - 1) {
-      let result = arr[0];
-      for (let i = 0; i < n - 1; i++) {
-        switch (selected[i]) {
-          case 0:
-            result += arr[i + 1];
-            break;
-          case 1:
-            result -= arr[i + 1];
-            break;
-          case 2:
-            result *= arr[i + 1];
-            break;
-          case 3:
-            result = parseInt(result / arr[i + 1]);
-            break;
-          default:
-            break;
-        }
-      }
+  let minValue = 1e9;
+  let maxValue = -1e9;
 
-      min = Math.min(min, result);
-      max = Math.max(max, result);
+  const dfs = (index, value) => {
+    if (index === n) {
+      minValue = Math.min(minValue, value);
+      maxValue = Math.max(maxValue, value);
       return;
     }
-    for (let i = 0; i < n - 1; i++) {
-      if (visited[i]) continue;
-      selected.push(flag[i]);
-      visited[i] = true;
-      dfs(depth + 1);
-      selected.pop();
-      visited[i] = false;
+    if (add > 0) {
+      add--;
+      dfs(index + 1, value + arr[index]);
+      add++;
+    }
+    if (sub > 0) {
+      sub--;
+      dfs(index + 1, value - arr[index]);
+      sub++;
+    }
+    if (mul > 0) {
+      mul--;
+      dfs(index + 1, value * arr[index]);
+      mul++;
+    }
+    if (div > 0) {
+      div--;
+      dfs(index + 1, ~~(value / arr[index]));
+      div++;
     }
   };
 
-  dfs(0);
-  console.log(max ? max : 0);
-  console.log(min ? min : 0);
+  dfs(1, arr[0]);
+
+  console.log(maxValue);
+  console.log(minValue);
 };
 
 solution(input);
