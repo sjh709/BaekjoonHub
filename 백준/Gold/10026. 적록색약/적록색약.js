e74f33e317b2939b5d67954475d69463188c9f7c
Jihee <42454759+sjh709@users.noreply.github.com>
2024-05-22 17:19:07 +0900

    [Gold V] Title: 적록색약, Time: 184 ms, Memory: 10780 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.js
+++ 백준/Gold/10026. 적록색약/적록색약.js
@@ -1,8 +1,5 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const dx = [-1, 1, 0, 0];
 const dy = [0, 0, -1, 1];
@@ -55,10 +52,4 @@ const solution = (input) => {
   console.log(answer.join(' '));
 };
 
-const input = [];
-rl.on('line', function (line) {
-  input.push(line);
-}).on('close', function () {
-  solution(input);
-  process.exit();
-});
+solution(input);
