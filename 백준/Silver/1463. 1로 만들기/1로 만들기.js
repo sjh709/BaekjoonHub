01f638d9f0f2c110089b16371a29154b0f740b35
Jihee <42454759+sjh709@users.noreply.github.com>
2024-05-30 03:04:34 +0900

    [Silver III] Title: 1로 만들기, Time: 212 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -0,0 +1,29 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function solution(input) {
+  const n = Number(input[0]);
+  let dp = new Array(n + 1).fill(0);
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + 1;
+    if (i % 2 === 0) {
+      dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+    }
+    if (i % 3 === 0) {
+      dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+    }
+  }
+  console.log(dp[n]);
+}
+
+const input = [];
+rl.on('line', function (line) {
+  input.push(line);
+  solution(input);
+  rl.close();
+}).on('close', function () {
+  process.exit();
+});
