3849a84c2286f891574a6fbf7c7bd6556ab9f4ef
Jihee <42454759+sjh709@users.noreply.github.com>
2024-06-02 03:52:51 +0900

    [Silver I] Title: 정수 삼각형, Time: 244 ms, Memory: 23464 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.js
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.js
@@ -1,8 +1,5 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
 const solution = (input) => {
   let n = Number(input[0]);
@@ -37,10 +34,4 @@ const solution = (input) => {
   console.log(Math.max(...dp[n - 1]));
 };
 
-const input = [];
-rl.on('line', function (line) {
-  input.push(line);
-}).on('close', function () {
-  solution(input);
-  process.exit();
-});
+solution(input);
