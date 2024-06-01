d55e7ae8b3b06dadc02f48824524eb76cec41aec
Jihee <42454759+sjh709@users.noreply.github.com>
2024-06-02 03:19:27 +0900

    [Silver III] Title: 파도반 수열, Time: 108 ms, Memory: 9836 KB -BaekjoonHub

--- 백준/Silver/9461. 파도반 수열/파도반 수열.js
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
+let dp = new Array(101).fill(0);
+dp[1] = 1;
+dp[2] = 1;
+dp[3] = 1;
+
+for (let i = 4; i <= 100; i++) {
+  dp[i] = dp[i - 2] + dp[i - 3];
+}
+
 const solution = (input) => {
   let t = Number(input[0]);
   for (let i = 1; i <= t; i++) {
     let n = Number(input[i]);
-    let dp = new Array(n + 1).fill(0);
-    dp[1] = 1;
-    dp[2] = 1;
-    dp[3] = 1;
-    dp[4] = 2;
-    for (let i = 5; i <= n; i++) {
-      dp[i] = dp[i - 5] + dp[i - 1];
-    }
     console.log(dp[n]);
   }
 };
