c28d22defdba8b57b8353e28f2063af139472099
Jihee <42454759+sjh709@users.noreply.github.com>
2024-06-18 14:37:14 +0900

    [Silver V] Title: 배열 합치기, Time: 1776 ms, Memory: 428100 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let [n, m] = input[0].split(' ').map(Number);
+let a = input[1].split(' ').map(Number);
+let b = input[2].split(' ').map(Number);
+
+let result = [];
+let i = 0;
+let j = 0;
+
+while (i < n && j < m) {
+  if (a[i] < b[j]) {
+    result.push(a[i]);
+    i += 1;
+  } else {
+    result.push(b[j]);
+    j += 1;
+  }
+}
+
+while (i < n) {
+  result.push(a[i]);
+  i += 1;
+}
+
+while (j < m) {
+  result.push(b[j]);
+  j += 1;
+}
+console.log(result.join(' '));
