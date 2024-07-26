c94c056717a6e9356f3d5572d86c0fb3a23e7f55
Jihee <42454759+sjh709@users.noreply.github.com>
2024-07-26 23:45:09 +0900

    [Silver III] Title: 합 구하기, Time: 424 ms, Memory: 46960 KB -BaekjoonHub

--- 백준/Silver/11441. 합 구하기/합 구하기.js
+++ 백준/Silver/11441. 합 구하기/합 구하기.js
@@ -1,30 +1,20 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const input = [];
-rl.on('line', function (line) {
-  input.push(line);
-}).on('close', function () {
-  const n = Number(input[0]);
-  const arr = input[1].split(' ').map(Number);
-  const m = Number(input[2]);
-  let sum = [0];
+const n = Number(input[0]);
+const arr = input[1].split(' ').map(Number);
+const m = Number(input[2]);
+let sum = [0];
 
-  for (let i = 0; i < n; i++) {
-    const result = sum[i] + arr[i];
-    sum.push(result);
-  }
+for (let i = 0; i < n; i++) {
+  const result = sum[i] + arr[i];
+  sum.push(result);
+}
 
-  let result = '';
-  for (let k = 3; k < 3 + m; k++) {
-    const [i, j] = input[k].split(' ').map(Number);
-    result += sum[j] - sum[i - 1] + '\n';
-  }
+let result = '';
+for (let k = 3; k < 3 + m; k++) {
+  const [i, j] = input[k].split(' ').map(Number);
+  result += sum[j] - sum[i - 1] + '\n';
+}
 
-  console.log(result);
-
-  process.exit();
-});
+console.log(result);
