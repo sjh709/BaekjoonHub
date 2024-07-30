1010ae4d4bd624e981becb7cf82d4da32ff688b0
Jihee <42454759+sjh709@users.noreply.github.com>
2024-07-30 22:13:34 +0900

    [Silver V] Title: 2차원 배열의 합, Time: 904 ms, Memory: 23440 KB -BaekjoonHub

--- 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.js
+++ 백준/Silver/2167. 2차원 배열의 합/2차원 배열의 합.js
@@ -1,38 +1,28 @@
-const readline = require('readline');
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
 
-const input = [];
-rl.on('line', function (line) {
-  input.push(line);
-}).on('close', function () {
-  const [n, m] = input[0].split(' ').map(Number);
-  const arr = [new Array(m + 1).fill(0)];
-  for (let i = 1; i <= n; i++) {
-    arr.push([0, ...input[i].split(' ').map(Number)]);
-  }
-  const k = Number(input[n + 1]);
-  const queries = [];
-  for (let line = n + 2; line < n + 2 + k; line++) {
-    const [i, j, x, y] = input[line].split(' ').map(Number);
-    queries.push([i, j, x, y]);
-  }
+const [n, m] = input[0].split(' ').map(Number);
+const arr = [new Array(m + 1).fill(0)];
+for (let i = 1; i <= n; i++) {
+  arr.push([0, ...input[i].split(' ').map(Number)]);
+}
+const k = Number(input[n + 1]);
+const queries = [];
+for (let line = n + 2; line < n + 2 + k; line++) {
+  const [i, j, x, y] = input[line].split(' ').map(Number);
+  queries.push([i, j, x, y]);
+}
 
-  const sum = [];
-  for (let i = 0; i <= n; i++) sum.push(new Array(m + 1).fill(0));
-  for (let i = 1; i <= n; i++) {
-    for (let j = 1; j <= m; j++) {
-      sum[i][j] = arr[i][j] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
-    }
+const sum = [];
+for (let i = 0; i <= n; i++) sum.push(new Array(m + 1).fill(0));
+for (let i = 1; i <= n; i++) {
+  for (let j = 1; j <= m; j++) {
+    sum[i][j] = arr[i][j] + sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1];
   }
+}
 
-  for (let index = 0; index < k; index++) {
-    const [i, j, x, y] = queries[index];
-    let current = sum[x][y] - sum[i - 1][y] - sum[x][j - 1] + sum[i - 1][j - 1];
-    console.log(current);
-  }
-
-  process.exit();
-});
+for (let index = 0; index < k; index++) {
+  const [i, j, x, y] = queries[index];
+  let current = sum[x][y] - sum[i - 1][y] - sum[x][j - 1] + sum[i - 1][j - 1];
+  console.log(current);
+}
