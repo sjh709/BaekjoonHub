a366bcb9cc0854598a0cd776c7745722b448b6c5
Jihee <42454759+sjh709@users.noreply.github.com>
2024-07-30 23:04:34 +0900

    [Gold II] Title: 컬러볼, Time: 832 ms, Memory: 99776 KB -BaekjoonHub

---
+++ 백준/Gold/10800. 컬러볼/컬러볼.js
@@ -0,0 +1,39 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const input = [];
+rl.on('line', function (line) {
+  input.push(line);
+}).on('close', function () {
+  let n = Number(input[0]);
+  let arr = [];
+  for (let i = 0; i < n; i++) {
+    let [c, s] = input[i + 1].split(' ').map(Number);
+    arr.push([c, s, i]);
+  }
+  arr.sort((a, b) => a[1] - b[1]);
+
+  let summary = 0;
+  let colorSummary = Array(200001).fill(0);
+  let result = Array(n).fill(0);
+
+  let start = 0;
+  while (start < n) {
+    let end = start;
+    while (end < n && arr[start][1] === arr[end][1]) end += 1;
+    for (let i = start; i < end; i++) {
+      result[arr[i][2]] = summary - colorSummary[arr[i][0]];
+    }
+    for (let i = start; i < end; i++) {
+      colorSummary[arr[i][0]] += arr[i][1];
+      summary += arr[i][1];
+    }
+    start = end;
+  }
+  console.log(result.join('\n'));
+
+  process.exit();
+});
